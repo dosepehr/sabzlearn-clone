d150e9ac42df3900a00d7a781f0cf869267e7a87
dosepehr <dosepehr@gmail.com>
2023-03-14 20:25:44 +0330

    add topbar links info

---
+++ src/constants/topbarData.js
@@ -0,0 +1,8 @@
+export const topbarLinks = [
+    { id: 1, title: 'آموزش ری اکت', href: '' },
+    { id: 2, title: 'آموزش پایتون', href: '' },
+    { id: 3, title: 'آموزش بوت استرپ', href: '' },
+    { id: 4, title: 'آموزش جاوا اسکریپت', href: '' },
+    { id: 5, title: 'آموزش CSS', href: '' },
+    { id: 6, title: 'آموزش HTML', href: '' },
+];
