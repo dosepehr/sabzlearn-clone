a0539772b073ab116a25236f857dffa968239c24
dosepehr <dosepehr@gmail.com>
2023-03-14 19:39:40 +0330

    start project!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 function App() {
-    return <>
-        
-    </>;
+    return (
+        <>
+            <b></b>
+            <h1 className='text-3xl font-bold underline text-green-400'>start project!</h1>
+            <b></b>
+        </>
+    );
 }
 
 export default App;
