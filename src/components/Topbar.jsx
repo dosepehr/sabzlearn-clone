00eba092c47f226aebbaea2fe710e886f79c019e
dosepehr <dosepehr@gmail.com>
2023-03-14 21:20:30 +0330

    add topbar icons

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,12 +1,28 @@
-import { topbarLinks } from '../constants/topbarData';
+import { topbarLinks, topbarIcons } from '../constants/topbarData';
 const Topbar = () => {
     return (
-        <div className='flex bg-[#00000024]'>
-            {topbarLinks.map((link) => (
-                <p className='text-topbatLinks mx-2 text-md' key={link.id}>
-                    {link.title}
-                </p>
-            ))}
+        <div className='bg-[#00000024]'>
+            <div className='lg:flex items-center justify-between container mx-auto 2xl:px-40 py-1 hidden'>
+                <div className='flex'>
+                    {topbarLinks.map((link) => (
+                        <p
+                            className='text-topbatLinks mx-2 text-md'
+                            key={link.id}
+                        >
+                            {link.title}
+                        </p>
+                    ))}
+                </div>
+                <div className='flex items-center'>
+                    <p className='text-white mx-2 font-semibold'>
+                        sabzlearn@gmail.com
+                    </p>
+                    {/* TODO make links tooltip */}
+                    {topbarIcons.map((icon) => (
+                        <span className='mx-1 text-white text-sm '>{icon.icon}</span>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
--- src/constants/topbarData.js
+++ src/constants/topbarData.js
@@ -1,8 +1,15 @@
+import { BsTelegram ,BsYoutube,BsTwitter,BsInstagram} from 'react-icons/bs';
 export const topbarLinks = [
-    { id: 1, title: 'آموزش ری اکت', href: '' },
-    { id: 2, title: 'آموزش پایتون', href: '' },
-    { id: 3, title: 'آموزش بوت استرپ', href: '' },
-    { id: 4, title: 'آموزش جاوا اسکریپت', href: '' },
-    { id: 5, title: 'آموزش CSS', href: '' },
     { id: 6, title: 'آموزش HTML', href: '' },
+    { id: 5, title: 'آموزش CSS', href: '' },
+    { id: 4, title: 'آموزش جاوا اسکریپت', href: '' },
+    { id: 3, title: 'آموزش بوت استرپ', href: '' },
+    { id: 2, title: 'آموزش پایتون', href: '' },
+    { id: 1, title: 'آموزش ری اکت', href: '' },
+];
+export const topbarIcons = [
+    { id: 4, icon: <BsInstagram />, tooltip: 'اینستاگرام' },
+    { id: 2, icon: <BsTwitter />, tooltip: 'توییتر' },
+    { id: 3, icon: <BsYoutube />, tooltip: 'یوتیوب' },
+    { id: 1, icon: <BsTelegram />, tooltip: 'تلگرام' },
 ];
