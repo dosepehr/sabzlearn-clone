3712a4c78f78a0edf29f1265fc334148325aa0ba
dosepehr <dosepehr@gmail.com>
2023-03-14 22:52:33 +0330

    edit Navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,17 @@ import { FaShoppingBasket } from 'react-icons/fa';
 import { BsFillPersonFill } from 'react-icons/bs';
 const Navbar = () => {
     return (
-        <div className='items-center justify-between container mx-auto 2xl:px-40 py-1 flex'>
+        <div className='items-center justify-between container mx-auto 2xl:px-40 py-2 flex border-b-[1px] border-b-white border-opacity-30 '>
             {/* TODO make ham menu and its button */}
             {/* TODO edit width lg */}
-            <p className='lg:hidden' >ham menu button</p>
+            <p className='lg:hidden'>ham menu button</p>
             <div className='flex items-center'>
-                <img src='/images/logo.png' alt='' className='h-10 mx-9'/>
+                <img src='/images/logo.png' alt='' className='h-10 mx-9' />
                 <div className='hidden lg:flex'>
                     {navbarData.map((link) => (
-                        <p key={link.id} className='mx-2' >{link.title}</p>
+                        <p key={link.id} className='mx-2 text-topbatLinks'>
+                            {link.title}
+                        </p>
                     ))}
                 </div>
             </div>
@@ -20,7 +22,7 @@ const Navbar = () => {
                     <p className='ml-2 hidden lg:block'>sepehr</p>
                     <BsFillPersonFill />
                 </div>
-                <div className='mr-2'>
+                <div className='mr-2 text-white text-xl'>
                     <FaShoppingBasket />
                 </div>
             </div>
