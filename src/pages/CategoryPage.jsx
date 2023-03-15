227e0455b052dd4e2aee381c83086c607ea4cd33
dosepehr <dosepehr@gmail.com>
2023-03-15 18:06:57 +0330

    make CategotyPage component

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,7 +1,34 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import { Topbar, Navbar, Footer, Courses } from '../components';
+import { navbarData } from '../constants/navbarData';
 const CategoryPage = () => {
     const { categoryName } = useParams();
-    return <div>{categoryName}</div>;
+    return (
+        <>
+            {/* add pagination */}
+            <div className='bg-secondaryColor'>
+                <Topbar />
+                <Navbar />
+            </div>
+            <div className='bg-primaryColor pt-5'>
+                <div className='max-w-[1080px] mx-auto px-12 space-y-3'>
+                    {navbarData.map(
+                        (link) =>
+                            link.href !== categoryName && (
+                                <Link
+                                    to={`/categories/${link.href}`}
+                                    className='text-topbatLinks block'
+                                >
+                                    {link.title}
+                                </Link>
+                            )
+                    )}
+                </div>
+            </div>
+            <Courses />
+            <Footer />
+        </>
+    );
 };
 
 export default CategoryPage;
