42521ef838b6a3fec9e7f8cf0f123deae0f3b823
dosepehr <dosepehr@gmail.com>
2023-03-15 19:25:56 +0330

    add Course main info

--- src/pages/CoursePage.jsx
+++ src/pages/CoursePage.jsx
@@ -2,11 +2,11 @@ import { Topbar, Navbar, Breadcrumb } from '../components';
 
 const CoursePage = () => {
     return (
-        <div>
-            <div className='bg-secondaryColor'>
-                <Topbar />
-                <Navbar />
-                <div className='flex justify-between '>
+        <div className='bg-secondaryColor'>
+            <Topbar />
+            <Navbar />
+            <div className='max-w-[1080px] mx-auto'>
+                <div className='flex justify-between max-[930px]:flex-col max-[930px]:text-center py-10'>
                     <p className='text-mainGreen'>
                         آموزش Next.js بصورت پروژه محور
                     </p>
@@ -19,6 +19,32 @@ const CoursePage = () => {
                             },
                         ]}
                     />
+                </div>
+                <div className='flex'>
+                    <div className='w-8/12 ml-20'>
+                        <img
+                            src='/images/sabz-next.png'
+                            alt=''
+                            className='w-full'
+                        />
+                    </div>
+                    <div className='w-4/12'>
+                        <p className='text-topbatLinks text-center'>
+                            850,000 تومان
+                        </p>
+                        <p className='bg-topbatLinks text-white py-2 px-1 rounded-md my-5'>
+                            پس از خرید، بلافاصله به محتوای دوره دسترسی خواهید
+                            داشت و میتوانید دوره را مشاهده و یا دانلود کنید.
+                        </p>
+                        <div className='text-topbatLinks'>
+                            <p>پــروژه مــحور بودن دوره هــــا</p>
+                            <p>پشتیبـــانی دائــــمی محصولات</p>
+                            <p>تضمین کیــفیت کلیـه محصولات</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex">
+
                 </div>
             </div>
         </div>
