ccda51c59c98a0ad4bea0053eba3273715a38998
dosepehr <dosepehr@gmail.com>
2023-04-03 21:15:33 +0430

    edit CourseBox component

--- src/components/CourseBox.jsx
+++ src/components/CourseBox.jsx
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom';
 import { Image, Shimmer } from 'react-shimmer';
-const CourseBox = () => {
+const CourseBox = ({ name, price, shortName }) => {
     return (
         <>
-            <Link to={`/course-info/npm`}>
+            <Link to={`/course-info/${shortName}`}>
                 <div className='w-full rounded-xl bg-secondaryColor overflow-hidden'>
                     <div className='h-full w-full'>
                         <Image
@@ -12,11 +12,11 @@ const CourseBox = () => {
                         />
                     </div>
                     <div className='px-5 pb-5'>
-                        <p className='text-white text-center my-6'>
-                            آموزش Next.js بصورت پروژه محور
-                        </p>
+                        <p className='text-white text-center my-6'>{name}</p>
                         <div className='flex items- justify-between'>
-                            <p className='text-white'>850000 تومان</p>
+                            <p className='text-white'>
+                                {price ? `${price} تومان` : 'رایگان'}
+                            </p>
                             <div className='flex flex-col items-center justify-center'>
                                 <p className='text-mainYellow font-semibold'>
                                     1020
