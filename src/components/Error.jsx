e89928b39f2f7bb3d34b244b8b64bd4e42f23049
dosepehr <dosepehr@gmail.com>
2023-05-24 22:27:56 +0330

    add Error component

---
+++ src/components/Error.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const Error = () => {
+    return (
+        <>
+            <div className='flex items-center justify-center  py-20 flex-col space-y-4'>
+                <p className='text-mainWhite text-6xl'>مشکلی پیش آمده...</p>
+                <p className='text-mainWhite text-3xl'>لطفا بعدا تلاش کنید</p>
+            </div>
+        </>
+    );
+};
+
+export default Error;
--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import 'react-tooltip/dist/react-tooltip.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { ErrorBoundary } from 'react-error-boundary';
-
+import Error from './components/Error';
 const root = createRoot(document.getElementById('root'));
 root.render(
     <StrictMode>
         <BrowserRouter>
-            <ErrorBoundary fallback={<div>Something went wrong</div>}>
+            <ErrorBoundary fallback={<Error />}>
                 <App />
             </ErrorBoundary>
         </BrowserRouter>
