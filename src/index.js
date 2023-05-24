9c546cf2888101e12b1e64f861c6dd8fc167c1d3
dosepehr <dosepehr@gmail.com>
2023-05-24 22:20:50 +0330

    add error boundary

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'react-tooltip/dist/react-tooltip.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { ErrorBoundary } from 'react-error-boundary';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <StrictMode>
         <BrowserRouter>
-            <App />
+            <ErrorBoundary fallback={<div>Something went wrong</div>}>
+                <App />
+            </ErrorBoundary>
         </BrowserRouter>
-    </React.StrictMode>
+    </StrictMode>
 );
-
